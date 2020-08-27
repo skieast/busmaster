30bc322684ef060013e6c1c9414e80ce08d783d6
Eric Evenchick <eric@evenchick.com>
2020-08-27 07:34:51 -0700

    re-add CANtact to DIL_CAN, fix build for CANtact

--- Sources/Kernel/BusmasterDriverInterface/DIL_CAN.cpp
+++ Sources/Kernel/BusmasterDriverInterface/DIL_CAN.cpp
@@ -67,6 +67,7 @@ enum
     DRIVER_CAN_VSCOM,
     DRIVER_CAN_IVIEW,
     DRIVER_CAN_ISOLAR,
+	DRIVER_CAN_CANTACT,
     DIL_TOTAL,          // Its value must be <= MAX_DILS
     DAL_NONE = ~0x0
 };
@@ -76,6 +77,7 @@ static ENTRY_DIL sg_ListDIL[] =
     /* simulation should be the first entry... */
     {DRIVER_CAN_STUB,       "&Simulation"       },
     /* ...all other drivers should be in alphabetical order */
+	{DRIVER_CAN_CANTACT,	"&CANtact"			},
     {DRIVER_CAN_ETAS_BOA,   "ETAS &BOA"         },
     {DRIVER_CAN_ETAS_ES581, "ETAS &ES581.3"     },
     {DRIVER_CAN_ETAS_ES5814, "ETAS ES581.&4"    },
@@ -354,6 +356,9 @@ HRESULT CDIL_CAN::DILC_SelectDriver(DWORD dwDriverID, HWND hWndOwner)
             case DRIVER_CAN_ISOLAR:
                 m_hDll = LoadLibrary("CAN_ISOLAR_EVE_VCAN.dll");
                 break;
+			case DRIVER_CAN_CANTACT:
+				m_hDll = LoadLibrary("CAN_CANtact.dll");
+				break;
             case DAL_NONE:
                 DILC_PerformClosureOperations();
                 vSelectInterface_Dummy();
